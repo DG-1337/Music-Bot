5b3a4cca5548d049424ba75423ca3e104cd74e01
DG-1337 <devgopal745@gmail.com>
2023-08-26 19:01:23 -0400

    Added slash commands and skip command

--- cogs/music.py
+++ cogs/music.py
@@ -9,6 +9,7 @@ class MusicBot(commands.Cog):
 
     vc : wavelink.Player = None
     node: wavelink.Node = None
+    track = None
 
     def __init__(self, bot):
         self.bot = bot
@@ -22,25 +23,42 @@ class MusicBot(commands.Cog):
     async def on_wavelink_node_ready(self, node: wavelink.Node):
         logger.info(f"[{node}] is ready")
 
-    @commands.command(brief="Joins user's voice channel")
-    async def join(self, ctx, player=wavelink.Player):
+    @commands.hybrid_command(brief="Joins user's voice channel")
+    async def join(self, ctx):
         channel = ctx.message.author.voice.channel
         self.music_channel = ctx.message.channel
         if not channel:
             await ctx.send(f"You need to join a voice channel first.")
             return 
-        self.vc = await channel.connect(cls=player)
-        await ctx.send(f"Joined {channel.name}")
+        self.vc = await channel.connect(cls=wavelink.Player)
+        await ctx.send(f"Joined {channel}", ephemeral=True)
 
-    @commands.command(brief="Search for a track on YouTube")
+    @commands.hybrid_command(name="play", description="Plays vid")
     async def play(self, ctx, *, title : str):
         # checks if bot is in a vc if not executes join command
         bot_vc = discord.utils.get(self.bot.voice_clients, guild = ctx.guild)
         if bot_vc == None:
             await ctx.invoke(self.bot.get_command("join"))
         chosen_track = await wavelink.YouTubeTrack.search(title, return_first=True)
-        await self.vc.play(chosen_track)
 
+        if chosen_track != None:
+            track = chosen_track
+            await self.vc.play(chosen_track)
+            await ctx.send(f"Now playing {chosen_track}", ephemeral=True)
+            return
+
+    @commands.hybrid_command(name="pause", desctription="Stops current track")
+    async def pause(self, ctx):
+        await self.vc.pause()
+        await ctx.send(f"Paused Current track", ephemeral=True)
+
+    # @commands.hybrid_command(name="skip", with_app_command=True)
+    # async def skip(self, ctx):
+    #     if self.vc.queue.is_empty:
+    #         await ctx.send("Queue is empty")
+    #         return
+    #     self.track = self.vc.queue.get()
+    #     await self.vc.play(self.track)
 
 async def setup(bot):
     music_bot = MusicBot(bot)
--- music_cogs.py
+++ music_cogs.py
@@ -6,15 +6,17 @@ import utils
 logger = settings.logging.getLogger("bot")
     
 def run():
+
     intents = discord.Intents.all()
-    
     bot = commands.Bot(command_prefix="!", intents=intents)
+    # sync slash commands
     
     @bot.event 
     async def on_ready():
         await utils.load_videocmds(bot)
         await bot.load_extension("cogs.music")
-
+        synced = await bot.tree.sync()
+        logger.info(f"Synced {len(synced)}, command(s)")
 
     @bot.command()
     async def load(ctx, cog: str):
@@ -22,15 +24,15 @@ def run():
         await ctx.send("Success")
         
     @bot.command()
-    async def unload(ctx, cog: str):
+    async def unload(ctx, interaction: discord.Interaction, cog: str):
         await bot.unload_extension(f"cogs.{cog.lower()}")
         await ctx.send("Success")
+
     @bot.command()
     async def reload(ctx, cog: str):
         await bot.reload_extension(f"cogs.{cog.lower()}")
         await ctx.send("Success")
 
-        
     bot.run(settings.DISCORD_API_SECRET, root_logger=True)
 
 if __name__ == "__main__":
